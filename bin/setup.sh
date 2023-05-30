823a3896fd18b8428f6704f85afe4df5696ff97c
Angger Pradana <sonypradana@gmail.com>
2023-05-30 16:12:43 +0700

    Update setup.sh
    adding `#!/bin/bash`

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,16 @@
-# simple stap setup project
+#!/bin/bash
+
+# Install PHP Dependencies
 composer install
+
+# Configure Environment
 cp .env.example .env
+
+# Install JavaScript Dependencies
 npm install
+
+# Build Frontend Assets
 npm run dev
 
-# builit cli
+# Run the CLI
 php cli --help
