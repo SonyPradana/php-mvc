666576d60c5243698c99561e9c5b6a12c439ecff
SonyPradana <sonypradana@gmail.com>
2024-03-21 20:20:21 +0700

    refactor: remove unuses js file

--- resources/js/app.js
+++
@@ -1,4 +0,0 @@
-// just short hand
-const $id = el => document.getElementById(el);
-const $ele = el => document.querySelector(el);
-const $click = (el, callback) => $ele(el).addEventListener('click', callback);
