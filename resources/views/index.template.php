6a4fe4920c43c530a95d155b104dab9292f69896
SonyPradana <sonypradana@gmail.com>
2024-03-21 20:19:22 +0700

    refactor: change welcome page card

--- resources/views/index.template.php
+++ resources/views/index.template.php
@@ -5,7 +5,7 @@
 {% section('content') %}
 <div class="dark:bg-gray-800 text-gray-900 m-auto w-9/12 bg-white dark:text-gray-100">
     <h1 class="text-3xl font-bold dark:text-gray-100" id="title">{{ $say }}</h1>
-    <p class="text-gray-700 dark:text-gray-300" id="time">PHP {{ phpversion() }}</p>
+    <p class="text-gray-700 dark:text-gray-300" id="time">php-library {{ \Composer\InstalledVersions::getPrettyVersion('sonypradana/php-library') }} (PHP v{{ phpversion() }})</p>
 
     <div class="grid grid-flow-row grid-cols-2 gap-4 py-2">
         <div class="p-2 min-h-24 rounded-md shadow-lg border-gray-700 bg-gray-200 dark:bg-gray-700">
@@ -21,8 +21,8 @@
             <div class="text-md text-gray-500 dark:text-gray-300">Build your costume API url is ready</div>
         </div>
         <div class="p-2 min-h-24 rounded-md shadow-lg border-gray-700 bg-gray-200 dark:bg-gray-700">
-            <div class="text-lg font-semibold text-gray-900 dark:text-gray-100">Support tailwind </div>
-            <div class="text-md text-gray-500 dark:text-gray-300">This page created using tailwind css.</div>
+            <div class="text-lg font-semibold text-gray-900 dark:text-gray-100">Template Engine</div>
+            <div class="text-md text-gray-500 dark:text-gray-300">Template engine is also available for crafting pages using your own templates.</div>
         </div>
     </div>
 </div>
