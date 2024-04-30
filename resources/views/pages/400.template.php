0b48ec7326464e9ac211eefdd1af3f85d671e8ff
SonyPradana <sonypradana@gmail.com>
2024-04-30 12:22:30 +0700

    feat: add more error pages
    - 400, 401, 403, 429, 500

--- resources/views/pages/400.template.php
+++ resources/views/pages/400.template.php
@@ -1,9 +1,9 @@
 {% extend('base/base.template.php') %}
 
-{% section('title', '405 | Method Not Allow') %}
+{% section('title', '400 | Bad Request') %}
 
 {% section('content') %}
 <div class="flex items-center justify-center w-screen h-screen bg-gray-50 dark:bg-gray-800">
-    <p class="text-xl text-gray-800 dark:text-gray-200 md:text-md lg:text-2xl">405 | Method Not Allow</p>
+    <p class="text-xl text-gray-800 dark:text-gray-100 md:text-md lg:text-2xl">400 | Bad Request</p>
 </div>
 {% endsection %}
