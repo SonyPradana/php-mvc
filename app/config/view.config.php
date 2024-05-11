60c257bed15b5df51c7b4c7910cb3b509c123b87
SonyPradana <sonypradana@gmail.com>
2024-05-11 11:48:29 +0700

    refactor: add `TemplaorFinder` and instance

---
+++ app/config/view.config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'VIEW_PATHS' => [
+        DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR,
+    ],
+    'VIEW_EXTENSIONS' => [
+        '.template.php',
+        '.php',
+        '',
+    ],
+    'COMPILED_VIEW_PATH' => DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR,
+];
