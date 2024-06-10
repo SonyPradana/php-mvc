90c4fc1855315ed87e6cf8453a71054e9154654c
SonyPradana <sonypradana@gmail.com>
2024-06-10 12:23:42 +0700

    refactor: change constructor paramter
    use `Application::class`  container instead `Container::class`

--- app/Karnels/ConsoleKernel.php
+++ app/Karnels/ConsoleKernel.php
@@ -4,45 +4,31 @@ declare(strict_types=1);
 
 namespace App\Karnels;
 
-use System\Container\Container;
+use System\Integrate\Application;
 use System\Integrate\Console\Karnel;
 use System\Integrate\ValueObjects\CommandMap;
 
 class ConsoleKernel extends Karnel
 {
-    public static $command = [];
-
     /** @var \Whoops\Run */
     private $run;
 
     /** @var \Whoops\Handler */
     private $handler;
 
-    public function __construct(Container $app)
+    public function __construct(Application $app)
     {
         parent::__construct($app);
 
-        /* @var \Whoops\Handler\PlainTextHandler */
-        $this->handler = $this->app->make('error.PlainTextHandler');
+        $this->app->bootedCallback(function () {
+            /* @var \Whoops\Handler\PlainTextHandler */
+            $this->handler = $this->app->make('error.PlainTextHandler');
 
-        /* @var \Whoops\Run */
-        $this->run = $app->make('error.handle');
-        $this->run
-          ->pushHandler($this->handler)
-          ->register();
-    }
-
-    /** {@inheritDoc} */
-    protected function commands()
-    {
-        $commands = static::$command = include config_path() . 'command.config.php';
-
-        /** @var CommandMap[] */
-        $maps = [];
-        foreach ($commands as $command) {
-            $maps[] = new CommandMap($command);
-        }
-
-        return $maps;
+            /* @var \Whoops\Run */
+            $this->run = $this->app->make('error.handle');
+            $this->run
+              ->pushHandler($this->handler)
+              ->register();
+        });
     }
 }
