394f7ae484f3a3f59410e13a10fa92730bb46ca8
SonyPradana <sonypradana@gmail.com>
2024-06-10 12:32:11 +0700

    refactor, feat: wrap config and cachable config

--- app/config/command.config.php
+++ app/config/command.config.php
@@ -1,14 +1,16 @@
 <?php
 
-return array_merge(
-    App\Commands\HelpCommand::$command,
-    App\Commands\CronCommand::$command,
-    System\Integrate\Console\MakeCommand::$command,
-    System\Integrate\Console\ServeCommand::$command,
-    System\Integrate\Console\RouteCommand::$command,
-    System\Integrate\Console\MigrationCommand::$command,
-    System\Integrate\Console\SeedCommand::$command,
-    System\Integrate\Console\ViewCommand::$command,
-    System\Integrate\Console\MaintenanceCommand::$command,
-    // more command here
-);
+return [
+    'commands' => [
+        App\Commands\HelpCommand::$command,
+        App\Commands\CronCommand::$command,
+        System\Integrate\Console\MakeCommand::$command,
+        System\Integrate\Console\ServeCommand::$command,
+        System\Integrate\Console\RouteCommand::$command,
+        System\Integrate\Console\MigrationCommand::$command,
+        System\Integrate\Console\SeedCommand::$command,
+        System\Integrate\Console\ViewCommand::$command,
+        System\Integrate\Console\MaintenanceCommand::$command,
+        System\Integrate\Console\ConfigCommand::$command,
+    ]
+];
