0d0796bfc8eaaa056839dd1b229b7616d9db2507
SonyPradana <sonypradana@gmail.com>
2024-06-26 21:58:35 +0700

    refactor: remove extends controller

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -2,21 +2,6 @@
 
 namespace App\Controllers;
 
-use System\Http\Response;
-use System\Router\Controller as BaseController;
-
-class Controller extends BaseController
+class Controller
 {
-    /**
-     * @param array<string, mixed> $portal
-     */
-    public static function renderView(string $view, array $portal = []): Response
-    {
-        return view($view, $portal);
-    }
-
-    public static function viewExists(string $view): bool
-    {
-        return file_exists(view_path() . $view . '.template.php');
-    }
 }
