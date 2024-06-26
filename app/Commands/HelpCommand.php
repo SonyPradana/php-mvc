6f3cf6a5f89b68563a40d62098d9ad36d34dde14
Angger Pradana <sonypradana@gmail.com>
2024-06-26 20:15:27 +0700

    refactor: return help command to default (except $benner) (#20)

--- app/Commands/HelpCommand.php
+++ app/Commands/HelpCommand.php
@@ -6,19 +6,6 @@ use System\Integrate\Console\HelpCommand as ConsoleHelpCommand;
 
 class HelpCommand extends ConsoleHelpCommand
 {
-    public static array $command = [
-        [
-            'cmd'       => ['-h', '--help'],
-            'fn'        => [self::class, 'main'],
-        ], [
-            'cmd'       => '--list',
-            'fn'        => [self::class, 'commandList'],
-        ], [
-            'cmd'       => 'help',
-            'fn'        => [self::class, 'commandhelp'],
-        ],
-    ];
-
     protected string $banner =
         '   _____ ____ _ _   __ ____ _ ____   ____   ____ _
   / ___// __ `/| | / // __ `// __ \ / __ \ / __ `/
