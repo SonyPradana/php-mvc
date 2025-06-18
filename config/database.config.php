747e5209953570cab376435b66c372b8ad3c27b9
sonypradana <sonypradana@gmail.com>
2025-06-18 22:36:21 +0700

    fix default db config

--- config/database.config.php
+++ config/database.config.php
@@ -8,24 +8,24 @@ return [
             'host'     => $_ENV['DB_HOST'] ?? 'localhost',
             'username' => $_ENV['DB_USER'] ?? 'root',
             'password' => $_ENV['DB_PASS'] ?? '',
-            'database' => $_ENV['DB_NAME'] ?? 'savanna',
+            'database' => $_ENV['DB_NAME'] ?? '',
             'port'     => (int) ($_ENV['DB_PORT'] ?? 3306),
             'chartset' => $_ENV['DB_CHARSET'] ?? 'utf8mb4',
-            'option'   => [
+            'options'   => [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES   => false,
             ],
         ],
         'mariadb' => [
-            'driver'   => 'mysql',
+            'driver'   => 'mariadb',
             'host'     => $_ENV['DB_HOST'] ?? 'localhost',
             'username' => $_ENV['DB_USER'] ?? 'root',
             'password' => $_ENV['DB_PASS'] ?? '',
             'database' => $_ENV['DB_NAME'] ?? '',
             'port'     => (int) ($_ENV['DB_PORT'] ?? 3306),
             'chartset' => $_ENV['DB_CHARSET'] ?? 'utf8mb4',
-            'option'   => [
+            'options'   => [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES   => false,
