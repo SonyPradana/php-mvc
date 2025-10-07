e7033f55aae83cdcf484e1383279ef3677cce872
Angger Pradana <sonypradana@gmail.com>
2025-10-07 20:58:38 +0700

    refactor: add support multiple database configuration and new database manager class (#34)
    * testing
    
    * add alias for deprecated cover
    
    * use new config structure
    
    * fix default db config
    
    * fix: ensure DB_CONNECTION is set in .env.example and use it in database config
    
    * bum sonypradana/php-library to ver 0.40
    
    * register `DatabaseManager` to container

--- app/Providers/DatabaseServiceProvider.php
+++ app/Providers/DatabaseServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use System\Database\DatabaseManager;
 use System\Database\MyPDO;
 use System\Database\MyQuery;
 use System\Database\MySchema;
@@ -13,24 +14,23 @@ class DatabaseServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $configs = $this->app->get('config');
-        $sql_dsn = [
-            'host'           => $configs['DB_HOST'],
-            'user'           => $configs['DB_USER'],
-            'password'       => $configs['DB_PASS'],
-            'database_name'  => $configs['DB_NAME'],
-        ];
+        $configs     = $this->app->get('config');
+        $default     = $configs['db.default'];
+        $connentions = $configs['db.connections'];
+        $dsn         = $connentions[$default];
 
-        $this->app->set('dsn.sql', $sql_dsn);
+        $this->app->set('dsn.default', $default);
+        $this->app->set('dsn.connenctions', $connentions);
+        $this->app->set('dsn.sql', $dsn);
 
         $this->app->set(
             MyPDO::class,
-            fn () => new MyPDO($sql_dsn)
+            fn () => new MyPDO($this->app->get('dsn.sql'))
         );
 
         $this->app->set(
             MySchema\MyPDO::class,
-            fn () => new MySchema\MyPDO($sql_dsn)
+            fn () => new MySchema\MyPDO($this->app->get('dsn.sql'))
         );
 
         $this->app->set(
@@ -40,7 +40,13 @@ class DatabaseServiceProvider extends ServiceProvider
 
         $this->app->set(
             'MySchema',
-            fn () => new MySchema($this->app->get(MySchema\MyPDO::class))
+            fn () => new MySchema($this->app->get(MySchema\MyPDO::class), $this->app->get('dsn.sql')['database'])
+        );
+
+        $this->app->set(
+            DatabaseManager::class,
+            fn () => (new DatabaseManager($this->app->get('dsn.connenctions')))
+                ->setDefaultConnection($this->app->get(MyPDO::class))
         );
     }
 }
--- config/database.config.php
+++ config/database.config.php
@@ -1,8 +1,61 @@
 <?php
 
 return [
-    'DB_HOST' => $_ENV['DB_HOST'] ?? 'localhost',
-    'DB_USER' => $_ENV['DB_USER'] ?? 'root',
-    'DB_PASS' => $_ENV['DB_PASS'] ?? '',
-    'DB_NAME' => $_ENV['DB_NAME'] ?? '',
+    'db.default'     => $_ENV['DB_CONNECTION'] ?? 'mysql',
+    'db.connections' => [
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => $_ENV['DB_HOST'] ?? 'localhost',
+            'username'  => $_ENV['DB_USER'] ?? 'root',
+            'password'  => $_ENV['DB_PASS'] ?? '',
+            'database'  => $_ENV['DB_NAME'] ?? '',
+            'port'      => (int) ($_ENV['DB_PORT'] ?? 3306),
+            'chartset'  => $_ENV['DB_CHARSET'] ?? 'utf8mb4',
+            'options'   => [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ],
+        ],
+        'mariadb' => [
+            'driver'    => 'mariadb',
+            'host'      => $_ENV['DB_HOST'] ?? 'localhost',
+            'username'  => $_ENV['DB_USER'] ?? 'root',
+            'password'  => $_ENV['DB_PASS'] ?? '',
+            'database'  => $_ENV['DB_NAME'] ?? '',
+            'port'      => (int) ($_ENV['DB_PORT'] ?? 3306),
+            'chartset'  => $_ENV['DB_CHARSET'] ?? 'utf8mb4',
+            'options'   => [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ],
+        ],
+        'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'     => $_ENV['DB_HOST'] ?? 'localhost',
+            'username' => $_ENV['DB_USER'] ?? 'root',
+            'password' => $_ENV['DB_PASS'] ?? '',
+            'database' => $_ENV['DB_NAME'] ?? '',
+            'port'     => (int) ($_ENV['DB_PORT'] ?? 5432),
+            'charset'  => $_ENV['DB_CHARSET'] ?? 'utf8',
+            'options'  => [
+                PDO::ATTR_CASE               => PDO::CASE_NATURAL,
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_ORACLE_NULLS       => PDO::NULL_NATURAL,
+                PDO::ATTR_STRINGIFY_FETCHES  => false,
+            ],
+        ],
+        'sqlite' => [
+            'driver'   => 'sqlite',
+            'database' => DIRECTORY_SEPARATOR . 'database' . DIRECTORY_SEPARATOR . ($_ENV['DB_DATABASE'] ?? 'database.sqlite'),
+            'options'  => [
+                PDO::ATTR_CASE               => PDO::CASE_NATURAL,
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_ORACLE_NULLS       => PDO::NULL_NATURAL,
+                PDO::ATTR_STRINGIFY_FETCHES  => false,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ],
+        ],
+    ],
 ];
